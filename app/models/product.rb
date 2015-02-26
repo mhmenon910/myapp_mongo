cdbb1b5030e1cfac2730ff51dad02b0cd2fe3c14
Manoj <nuventure@nuventure-Satellite-C660.(none)>
2015-02-26 13:58:32 +0530

    validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,13 @@
 class Product
+
   include Mongoid::Document
   field :name, type: String
   field :description, type: String
   field :price, type: Float
+validates_presence_of :name
 end
+
+
+
+
+
